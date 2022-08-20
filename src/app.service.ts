05744968cd7c712ae05a9f49cf4b3cdf9a54fa07
birdgang82 <birdgang82@gmail.com>
2022-08-20 18:59:04 +0900

    devops update

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'DEVOPS, CI&CD DEPLOY FROM ECS - UPDATE';
+    return 'DEVOPS, CI&CD DEPLOY FROM ECS - UPDATE - 2';
   }
 }
